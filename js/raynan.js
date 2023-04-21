335781f4268fece638646049ff58239186a85794
RayAra2004 <raynan.araujo@gmail.com>
2023-04-21 19:46:13 -0300

    Navegação Pós Quizz - Reiniciar Quizz

--- js/raynan.js
+++ js/raynan.js
@@ -1,6 +1,6 @@
 axios.defaults.headers.common['Authorization'] = 'q2ieOg8Y10hTP78k4DXPL5S4';
 
-let quizz;
+let quizz, qtdAcertos = 0, qtdPeguntas = 0, qtdPerguntasRespondidas = 0;
 let respostas = [];
 
 function renderizaQuizz(){
@@ -10,7 +10,8 @@ function renderizaQuizz(){
     <p>${quizz.title}</p>`;
 
     const divPerguntas = document.querySelector('.perguntas');
-    
+    divPerguntas.innerHTML = '';
+    qtdPeguntas = Number(quizz.questions.length);
     quizz.questions.forEach(question =>{
         divPerguntas.innerHTML += `
             
@@ -42,34 +43,71 @@ function renderizaQuizz(){
 
 }
 
+function exibiResultado(){
+    const porcetagemAcertos = Math.ceil(qtdAcertos / qtdPeguntas * 100);
+    const divResultado = document.querySelector('.resultadoQuizz');
+    setTimeout(function(){
+        divResultado.style.display = 'flex';
+        divResultado.scrollIntoView();
+    }, 2000);
+    let nivel;
+    for(let i = 0; i < quizz.levels.length; i++){
+        if(i === quizz.levels.length - 1){
+            nivel = quizz.levels[i];
+            break
+        } else if(quizz.levels[i].minValue >= porcetagemAcertos && porcetagemAcertos < quizz.levels[i + 1].minValue){
+            nivel = quizz.levels[i];
+            break
+        }
+    }
+    const resultado = divResultado.querySelector('.qtdAcertos');
+    resultado.innerHTML = `
+        <p class="title_level"> ${porcetagemAcertos}% de acerto:${nivel.title}</p>
+    `;
+    const mensagem = divResultado.querySelector('.mensagem');
+    mensagem.innerHTML = `
+        <img src="${nivel.image}" alt="">
+        <p>${nivel.text}</p>
+    `
+}
+
 function verificarResposta(res){
-    const divRespostas = res.parentElement.querySelectorAll('.resposta');
-    divRespostas.forEach(r =>{
-        r.removeAttribute('onclick');
-        if(r.innerHTML !== res.innerHTML){
-            r.classList.add('esbranquicado');          
-        }
-        let p = r.querySelector('p');
-        if(r.getAttribute('res') === 'false'){
-            p.classList.add('respostaErrada');
-        } else{
-            p.classList.add('respostaCorreta');
-        }
-    });
-    const proximaPergunta = res.parentElement.parentElement.nextElementSibling;
-    if(proximaPergunta !== null){
-        setTimeout(function(){
-            proximaPergunta.scrollIntoView();
-        }, 2000);
+    if(!(res.classList.contains('respondido'))){
+        const divRespostas = res.parentElement.querySelectorAll('.resposta');
         
-    };
+        qtdPerguntasRespondidas++;
+        if(res.getAttribute('res') === 'true'){
+            qtdAcertos++;
+        }
+        divRespostas.forEach(r =>{
+            r.classList.add('respondido');
+            if(r.innerHTML !== res.innerHTML){
+                r.classList.add('esbranquicado');          
+            }
+            let p = r.querySelector('p');
+            if(r.getAttribute('res') === 'false'){
+                p.classList.add('respostaErrada');
+            } else{
+                p.classList.add('respostaCorreta');
+            }
+        });
+        const proximaPergunta = res.parentElement.parentElement.nextElementSibling;
+        if(proximaPergunta !== null){
+            setTimeout(function(){
+                proximaPergunta.scrollIntoView({behavior:"smooth"});
+            }, 2000);     
+        }
+        if(qtdPeguntas === qtdPerguntasRespondidas){
+            exibiResultado();
+            console.log('olaaaaaa')
+        };
+    }
 }
 
 function capturaQuizz(){
     const promise = axios.get('https://mock-api.driven.com.br/api/vm/buzzquizz/quizzes/3'); //TODO: Alterar, pois está capturando 1 quizz
     promise.then(res =>{
         quizz = res.data;
-        console.log(quizz);
         renderizaQuizz();
     });
 
@@ -80,6 +118,26 @@ function capturaQuizz(){
 
 function comparador() { 
 	return Math.random() - 0.5; 
+}
+
+function reiniciaQuizz(){
+    const divTitle = document.querySelector('.title');
+    divTitle.scrollIntoView();
+
+    const divResultado = document.querySelector('.resultadoQuizz');
+    divResultado.style.display = 'none';
+    
+    document.querySelectorAll('.esbranquicado').forEach( e => e.classList.remove('esbranquicado') );
+    document.querySelectorAll('.respostaErrada').forEach( e => e.classList.remove('respostaErrada') );
+    document.querySelectorAll('.respostaCorreta').forEach( e => e.classList.remove('respostaCorreta') );
+    document.querySelectorAll('.respondido').forEach( e => e.classList.remove('respondido') );
+    
+    qtdAcertos = 0;
+    qtdPeguntas = 0;
+    qtdPerguntasRespondidas = 0;
+
+    capturaQuizz();
+
 }
 
 
