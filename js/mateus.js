b58e97c7547cc3035ccf91e1fc2c25dcb236eac3
Mateus Faissal <mateusfaissal@gmail.com>
2023-04-23 03:01:06 -0300

    Layout TELA1 finalizado, JSprecisa de ajustes

---
+++ js/mateus.js
@@ -0,0 +1,40 @@
+axios.defaults.headers.common['Authorization'] = 'q2ieOg8Y10hTP78k4DXPL5S4';
+let quizzes;
+let quizz = [];
+
+
+
+function renderizaQuizz() {
+    const divQuizzes = document.querySelector('.quizz-row');
+    divQuizzes.innerHTML = "";
+
+    for (let i = 0; i < quizz.length; i++) {
+        quizzes = quizz[i];
+        console.log(quizzes);
+        divQuizzes.innerHTML = `<div class="quizz">
+    <img src="${quizzes.image}">
+    <p> ${quizzes.title} </p>
+    </div>` 
+    }  
+ 
+
+}
+
+
+
+
+function obterQuizz() {
+    
+    const promessa = axios.get('https://mock-api.driven.com.br/api/vm/buzzquizz/quizzes');
+
+    promessa.then(res=>{
+        quizz = res.data
+        renderizaQuizz();
+        console.log(quizz);
+    });
+    promessa.catch(erro => {
+        alert('Erro ao buscar quizzes no servidor, tente novamente mais tarde!');
+    });
+}
+
+obterQuizz();
